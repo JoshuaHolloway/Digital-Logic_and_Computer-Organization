ceed5aee4ab40ab87eed5cf62be02afe71ce4201
joshuaholloway <jhollow6@asu.edu>
2019-04-25 14:13:50 -0700

    Added toy program

---
+++ assembly/01_add.asm
@@ -0,0 +1,7 @@
+.data
+	number1: .word 5
+	number2: .word 10
+.text
+	lw $t0, number1
+	lw $t1, number2
+	add $t2, $t0, $t1 # t2 := t0 + t1
---
+++ processor/readme.txt
@@ -0,0 +1,2 @@
+program_00:
+    lw $s3, -24($s4)    # Machine: 0x8E93FFe8
